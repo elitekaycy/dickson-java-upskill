2b27e79cc01f0cf9143ae9fe4f4f92b2ad88d448
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-04 10:56:01 +0000

    feat: added mortgage calculator

--- src/amalitech/MortgageCalculator.java
+++ src/amalitech/MortgageCalculator.java
@@ -1,7 +1,7 @@
 package amalitech;
 
-import javax.sound.midi.Soundbank;
 import java.text.NumberFormat;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,71 +15,85 @@ import java.util.Scanner;
  */
 
 public class MortgageCalculator {
+    private static final byte MONTHS_IN_YEAR=12;
+    private static final byte PERCENT=100;
+    int principal;
+    double annualInterestRate;
+    byte years;
 
-    /**
-     * Now It’s time for a small project:
-     * Use what you’ve learned in the section and build a mortgage calculator
-     * When we run this program we get fews questions:
-     * Principal – Amount Of Loan We want to get – $ 100000
-     * Annual Interest Rate: 3.92 will be devided by 100 then /12
-     * Period(Years)- 30 to be * 12 – number of payments
-     * Add Erro Handling!!
-     * */
-    public static void main(String...args){
 
-        final byte MONTHS_IN_YEAR=12;
-        final byte PERCENT=100;
-        int principal=0;
+    public MortgageCalculator(int principal, float annualInterestRate, byte years) {
+        this.principal = principal;
+        this.annualInterestRate = annualInterestRate;
+        this.years = years;
+    }
+
+    private double calculateMortgage() {
+        float monthlyInterest = this.getMonthlyInterest();
+        int numberOfPayments = this.getNumberOfPayments();
+
+        return this.principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    }
+
+    public void displayMortgage() {
+        double getMortgage = this.calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(getMortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+
+    public float getMonthlyInterest() {
+        return (float) (this.annualInterestRate / PERCENT / MONTHS_IN_YEAR);
+    }
+    public int getNumberOfPayments() {
+        return this.years * MONTHS_IN_YEAR;
+    }
+
+    public static void main(String...args) {
+        int principal;
+        float annualInterestRate;
+        byte years;
         Scanner scanner=new Scanner(System.in);
 
-        //How Could We Add The Error handling??
-        while(true) {
+        while (true) {
             System.out.println("Principal:");
-            //The Scope Of variable principle should be declared outside the while block? Why?
-            principal = scanner.nextInt();
-            if(principal>1000 && principal<=1_000_000)
-            break;
-            System.out.println("Enter A Value Between 1000 and 1000000");
+            try {
+                principal = scanner.nextInt();
+                if (principal < 1000 || principal >= 1_000_000) throw new MortgageException("Enter a value between 1000 and 10000000");
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }
         }
 
-        //How Can we Add The Error handling Here
-        System.out.println("Annual Interest Rate:");
-        float annualInterestRate=scanner.nextFloat();
-        //We need to avoid any implicit value: 4,6 for code readability? Meaningfull & Descriptive Words
-        float monthlyInterest=annualInterestRate/PERCENT/MONTHS_IN_YEAR;
+        while(true) {
+            System.out.println("Annual Interest Rate:");
+            try {
+                annualInterestRate= (float) scanner.nextFloat();
+                if(annualInterestRate < 0) throw new MortgageException("interest should be more than O");
+                break;
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }
+        }
 
-        //How can We Add The Error Handling Here?
-        System.out.println("Period(Years):");
-        byte years=scanner.nextByte();
-        int numberOfPayments=years*MONTHS_IN_YEAR;
+        while (true) {
+            System.out.println("Period(Years):");
+            try {
+                years = scanner.nextByte();
+                if (years < 0) throw new MortgageException("years should be more than 0");
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }
+        }
 
-        //How can We Add The Error Handling Here?
-        double mortgage=principal*(monthlyInterest*Math.pow(1+monthlyInterest,numberOfPayments))/(Math.pow(1+monthlyInterest,numberOfPayments)-1);
-        String mortgageFormatted= NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage:"+mortgageFormatted);
-
-        //Challenge #2 - Let's increase the readability of  this code
-        //Does this program has a number of problem : Input validation???
-        //What if You Pass Unexpected Input??String?? or negative values?
-        //It's now time to use <<Conditional statement now come help us to solve this>>
-
-        // Next Challenge: when codes start getting too long, it hurts the maintainability!
-        //We need to convert the code into smaller and easier chunks using OOP.
-        //Build On This Mortgage Calculator To Enhance The Code Using OOP Best Practices And SOLID Principles
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        MortgageCalculator calculator = new MortgageCalculator(principal, annualInterestRate, years);
+        calculator.displayMortgage();
     }
 }
